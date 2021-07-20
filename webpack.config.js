acf979560a92e14c38f8d9830401da13f311fc21
jn_xyp <jinan.xyp@gmail.com>
2021-07-20 22:55:04 +0800

    Using webpack for LESS compiling and css postprocess; Separate different modules to different less files; Updated web fonts definition; Re-write ss-button classes with clip-path; Added color variables based on status (success, danger...).

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: {"ss_css": "./src/ss_css.js"},
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })],
+    module: {
+        rules: [
+            {
+                test: /\.(less)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "less-loader"
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: "assets/images/[hash].[ext]"
+                        }
+                    }]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: "assets/fonts/[hash].[ext]"
+                        }
+                    }]
+            }
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    devtool: "source-map"
+};
